5cd9d32d5f1f9ea8aa303bebf96014b8f53e91f6
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-04-28 10:01:19 -0500

    fix: env

--- api/src/env.js
+++ api/src/env.js
@@ -1,6 +1,8 @@
 const environments = {
   PORT: process.env.PORT || '3000',
-  MONGO_URI: process.env.MONGO_URI || 'URI',
+  MONGO_URI:
+    process.env.MONGO_URI ||
+    'mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]',
 }
 
 module.exports = environments
