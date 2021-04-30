1a0b12bf8cde5ced7293972ebc9e04a1c6f765c5
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-04-30 14:58:13 -0500

    feat: add signIn and signUp

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -1,20 +1,73 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const createError = require('http-errors')
+
+const User = require('../models/user')
+
 const validate = require('../lib/validate')
 const { signInSchema, signUpSchema } = require('../validations/auth')
+
+const {
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRES_IN,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRES_IN,
+} = require('../env')
 
 // signIn
 
 const signInValidation = validate.body(signInSchema)
 
-const signIn = (req, res) => {
-  res.json({})
+const signIn = async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (!user || !(await bcrypt.compare(req.body.password, user?.password)))
+    return next(createError(401, 'Incorrect email or password'))
+
+  user.password = undefined
+
+  const access_token = await jwt.sign(
+    { _id: user._id, updatedAt: user.updatedAt.getTime() },
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: ACCESS_TOKEN_EXPIRES_IN }
+  )
+  const refresh_token = await jwt.sign(
+    { _id: user._id },
+    REFRESH_TOKEN_SECRET + user.updatedAt.getTime(),
+    { expiresIn: REFRESH_TOKEN_EXPIRES_IN }
+  )
+
+  res.json({ user, access_token, refresh_token })
 }
 
 // signUp
 
 const signUpValidation = validate.body(signUpSchema)
 
-const signUp = (req, res) => {
-  res.json({})
+const signUp = async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (user) return next(createError(409, 'Email is already in use'))
+
+  const salt = await bcrypt.genSalt(10)
+  const password = await bcrypt.hash(req.body.password, salt)
+
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password,
+  })
+
+  const access_token = await jwt.sign(
+    { _id: newUser._id, updatedAt: newUser.updatedAt.getTime() },
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: ACCESS_TOKEN_EXPIRES_IN }
+  )
+  const refresh_token = await jwt.sign(
+    { _id: newUser._id },
+    REFRESH_TOKEN_SECRET + newUser.updatedAt.getTime(),
+    { expiresIn: REFRESH_TOKEN_EXPIRES_IN }
+  )
+
+  res.json({ user: newUser, access_token, refresh_token })
 }
 
 module.exports = {
--- api/src/env.js
+++ api/src/env.js
@@ -3,6 +3,11 @@ const environments = {
   MONGO_URI:
     process.env.MONGO_URI ||
     'mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]',
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || 'ACCESS_TOKEN_SECRET',
+  ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN || '1d',
+  REFRESH_TOKEN_SECRET:
+    process.env.REFRESH_TOKEN_SECRET || 'REFRESH_TOKEN_SECRET',
+  REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN || '3d',
 }
 
 module.exports = environments
