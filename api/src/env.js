7ccd9e71dba085f4e5e86aa9d7d05a1dd295e955
Santiago Gómez Solarte <santiagogomezsolarte@gmail.com>
2021-05-03 19:07:22 -0500

    test: test

--- api/src/env.js
+++ api/src/env.js
@@ -1,8 +1,6 @@
 const environments = {
   PORT: process.env.PORT || '3000',
-  MONGO_URI:
-    process.env.MONGO_URI ||
-    'mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/TicTacToe',
 }
 
 module.exports = environments
--- api/src/server.js
+++ api/src/server.js
@@ -11,6 +11,55 @@ require('./database')
 
 // Server
 
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   console.log(`> Server running on port ${PORT}`)
+})
+
+//Models database
+
+const User = require('./models/user')
+
+//Sockets
+
+const socketio = require('socket.io')
+const io = socketio(server)
+
+//Vars settings
+
+var players = {}
+
+io.on('connection', async (socket) => {
+  //Players
+
+  socket.on('new-player', async (data) => {
+    if (players.session) {
+      players.session.username = data.username
+      players.session.email = data.email
+      players.session.password = data.password
+    } else {
+      players.session = data.session
+      players.session.username = data.username
+      players.session.email = data.email
+      players.session.password = data.password
+    }
+  })
+
+  socket.on('player-exit', async () => {})
+
+  socket.on('player-won', async (data) => {
+    players.session.email = data.email
+    players.session.score = data.score
+
+    const player_score = await User.findOne({ email: players.email })
+
+    if (!player_score) {
+      User.updateOne({ email: players.email }, { score: players.score })
+    }
+  })
+
+  //Game
+
+  socket.on('new-room', async () => {})
+
+  socket.on('update-list-room', async () => {})
 })
