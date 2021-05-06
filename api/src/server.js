af01ba081c0487365d33378bde47e8c00425d5b8
Santiago Gómez Solarte <santiagogomezsolarte@gmail.com>
2021-05-06 15:48:34 -0500

    test: sockets

--- api/src/server.js
+++ api/src/server.js
@@ -15,10 +15,6 @@ const server = app.listen(PORT, async () => {
   console.log(`> Server running on port ${PORT}`)
 })
 
-//Models database
-
-const User = require('./models/user')
-
 //Sockets
 
 const socketio = require('socket.io')
@@ -44,22 +40,22 @@ io.on('connection', async (socket) => {
     }
   })
 
-  socket.on('player-exit', async () => {})
-
-  socket.on('player-won', async (data) => {
-    players.session.email = data.email
-    players.session.score = data.score
-
-    const player_score = await User.findOne({ email: players.email })
-
-    if (!player_score) {
-      User.updateOne({ email: players.email }, { score: players.score })
-    }
-  })
-
   //Game
 
-  socket.on('new-room', async () => {})
+  socket.on('new-room', async (data) => {
+    if (rooms.id == data.id) {
+      //Set data in rooms.id = {} object
+
+      var rooms = (id, name) => {
+        rooms[id] = {}
+        rooms[id].name = name
+      }
+
+      rooms(data.id, data.name)
+
+      socket.emit('room-' + data.id)
+    }
+  })
 
   socket.on('update-list-room', async () => {})
 })
