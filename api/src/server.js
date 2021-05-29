ff7365804d9f5987273c41efb8cc8e8d48c002a0
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-05-29 13:27:49 -0500

    feat: cors client

--- api/src/server.js
+++ api/src/server.js
@@ -28,7 +28,12 @@ const server = app.listen(PORT, async () => {
 
 // Sockets
 
-const io = require('socket.io')(server, {})
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+})
 
 io.on('connection', (socket) => {
   socket.on('signIn', async ({ id, name }) => {
