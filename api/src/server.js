0b648f0a6ab2b65baf16163170f53dc5699bf5ff
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-05-29 12:05:33 -0500

    fix: delete cors

--- api/src/server.js
+++ api/src/server.js
@@ -28,12 +28,7 @@ const server = app.listen(PORT, async () => {
 
 // Sockets
 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-})
+const io = require('socket.io')(server, {})
 
 io.on('connection', (socket) => {
   socket.on('signIn', async ({ id, name }) => {
