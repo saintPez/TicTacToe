a1c5bb5723043e701e0ab6a60ca469108c22d200
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-06-05 17:04:55 -0500

    fix: account component

--- app/src/components/Account/index.js
+++ app/src/components/Account/index.js
@@ -13,9 +13,7 @@ function Account() {
             {(user.username || user.name)?.charAt(0)}
           </span>
           <div className="main-account-info">
-            <div className="main-account-info-name">
-              {user.username || user.name}
-            </div>
+            <div className="main-account-info-name">{user.name}</div>
             <Link to="/account">Account</Link>
             <br />
             <Link to="/settings">Settings</Link>
