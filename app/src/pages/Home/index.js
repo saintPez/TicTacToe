f5b03096e9492d6b1d566198c0a58af7295630fb
Santiago <santiagodavidlopezbenavides@gmail.com>
2021-06-08 11:54:48 -0500

    fix: home page

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -14,72 +14,39 @@ function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const items_model = document.getElementsByClassName('item-content')
-
-  const handleClickOn = (e) => {
-    let val = e.target.getAttribute('list-value')
-    val = document.getElementById(val)
-
-    for (let i = 0; i < items_model.length; i++) {
-      items_model[i].style.display = 'none'
-      val.style.display = 'block'
-    }
-  }
-
   return (
     <>
       <div className="main-item">
-        <ul className="list-menu">
-          <li className="item-model" list-value="about" onClick={handleClickOn}>
-            About
-          </li>
-          <li
-            className="item-model"
-            list-value="policy"
-            onClick={handleClickOn}
-          >
-            Privacy Policy
-          </li>
-          <li className="item-model" list-value="terms" onClick={handleClickOn}>
-            Terms of use
-          </li>
-        </ul>
-        <div className="menu-load">
-          <div id="about" className="item-content show">
-            TicTacToe es una aplicación de juego, inspirada en tres en raya o
-            tres en línea. Que te permite compartir con amigos. Crear juegos
-            personalizados, donde puedes jugar solo y jugar en línea.
-            <br />
-            TicTacToe además permite que hables con tus amigos por el medio del
-            Chat mientras juegas, este chat es a nivel global. Para que puedas
-            interactuar con tus amigos y con otras personas dentro de la
-            prataforma.
-          </div>
-          <div id="policy" className="item-content">
-            En la política de privacidad de TicTacToe, prometemos proteger la
-            información de nuestros usuarios.
-            <br />
-            Además toda la información que recopilamos sea nombre, correo
-            eléctronico y otros aspectos más relacionados con el historial de
-            cada partida jugada o aspectos del juego. Son usados para mejorar
-            nuestros servicios y no hacemos uso de ellos para fines lucrativos.
-          </div>
-          <div id="terms" className="item-content">
-            Nuestros términos de uso resaltan los siguientes puntos para cada
-            usuario.
-            <br />
-            <ol className="m-p">
-              <li>
-                Si usted es parte de TicTacToe, debe respetar a los miembros de
-                la comunidad.
-              </li>
-              <li>
-                Si usted habla desde el chat, deberá ser respetuoso con los
-                demás miembros.
-              </li>
-            </ol>
-          </div>
-        </div>
+        <h1>News</h1>
+        <p>
+          TicTacToe is a game application, inspired by tic-tac-toe online. It
+          allows you to share with friends. Create personalized games, where you
+          can play alone and play online. TicTacToe also allows you to chat with
+          your friends while you play, this chat is global. So you can interact
+          with your friends and other people within the platform.
+        </p>
+        <br />
+        <p>
+          In TicTacToe's privacy policy, we promise to protect our users'
+          information. In addition all the information we collect whether it is
+          name, email and other aspects related to the history of each game
+          played or aspects of the game. They are used to improve our services
+          and we do not use them for profit.
+        </p>
+        <br />
+        <p>
+          Our terms of use highlight the following points for each user user
+          <ol className="m-p">
+            <li>
+              If you are part of TicTacToe, you must respect the members of the
+              community. the community.
+            </li>
+            <li>
+              If you speak from the chat, you must be respectful of other
+              members.
+            </li>
+          </ol>
+        </p>
       </div>
     </>
   )
